4dab21670351b80c7d1a599829e4d7ccc83b3d29
Neel Raval <97961627+NeelRavalt@users.noreply.github.com>
2023-12-21 17:37:20 +0530

    event-binding

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { ViewEncapsulation } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+
+})
+export class AppComponent {
+
+  clicks(){console.log('you cliked button')}
+   
+  getdata(){
+    alert('sorry data is not available')
+  }
+}
