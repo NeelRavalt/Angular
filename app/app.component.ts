101c561c1a1164f767409fe423a07404ab2ce5b1
Neel Raval <97961627+NeelRavalt@users.noreply.github.com>
2023-12-22 09:43:16 +0530

    Two-way -databinding

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { ViewEncapsulation } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+
+})
+export class AppComponent {
+  title = 'practice-2';
+  name:string = 'neel nilay'
+  
+
+}
